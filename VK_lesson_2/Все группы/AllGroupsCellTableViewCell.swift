312f0ecccb1a970f548ab0b01925dac1cf8bddea
r.chernetsov <r.chernetsov@adonis24.ru>
2019-01-12 15:53:42 +0300

    add classes

---
+++ VK_lesson_2/Все группы/AllGroupsCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AllGroupsCellTableViewCell.swift
+//  VK_lesson_2
+//
+//  Created by Чернецова Юлия on 12/01/2019.
+//  Copyright © 2019 Чернецов Роман. All rights reserved.
+//
+
+import UIKit
+
+class AllGroupsCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ VK_lesson_2/Фото/FiendFotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FiendFotoCell.swift
+//  VK_lesson_2
+//
+//  Created by Чернецова Юлия on 12/01/2019.
+//  Copyright © 2019 Чернецов Роман. All rights reserved.
+//
+
+import UIKit
+
+class FiendFotoCell: UICollectionViewCell {
+    
+}
