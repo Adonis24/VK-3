9f6d2516a814bb5f971c378131e883dea7f8200f
r.chernetsov <r.chernetsov@adonis24.ru>
2019-01-13 13:26:40 +0300

    add filter

--- VK_lesson_2/Все группы/AllGroupsTableViewController.swift
+++ VK_lesson_2/Все группы/AllGroupsTableViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class AllGroupsTableViewController: UITableViewController {
- var allGroups = ["Актеры","Композиторы","Автомобили","Спорт","Путешествие","Экстрим","Политика"]
+extension AllGroupsTableViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+    }
+}
+class AllGroupsTableViewController: UITableViewController,UISearchBarDelegate {
+ 
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var allGroups = ["Актеры","Композиторы","Автомобили","Спорт","Путешествие","Экстрим","Политика"]
     var allGroupsFoto = ["Актеры":"Actors","Композиторы":"Composers","Автомобили":"Сars","Спорт":"Sport","Путешествие":"Travel","Экстрим":"Extrim","Политика":"Polit"]
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredAllGroups: [String] = []
+    var searchActive : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Setup the Search Controller
+//        searchController.searchResultsUpdater = self as? UISearchResultsUpdating
+//        searchController.obscuresBackgroundDuringPresentation = false
+//        searchController.searchBar.placeholder = "Поиск группы"
+//        navigationItem.searchController = searchController
+//        definesPresentationContext = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredAllGroups = allGroups.filter({(text) -> Bool in
+            let tmp: NSString = text as NSString
+            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        // Filter the data array and get only those countries that match the search text.
+//                filteredAllGroups = allGroups.filter({ (searchText) -> Bool in
+//                    let groupsMatch: NSString = searchText as NSString
+//
+//                    return (groupsMatch.range(of: searchText, options: NSString.CompareOptions.caseInsensitive).location) != NSNotFound
+//                })
+        
+        searchActive = true
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
@@ -31,6 +66,10 @@ class AllGroupsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if searchActive{
+            return filteredAllGroups.count
+        }
+        
         return allGroups.count
     }
 
@@ -38,7 +77,12 @@ class AllGroupsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "allGroupsCell", for: indexPath) as! AllGroupsCellTableViewCell
-        let name = allGroups[indexPath.row]
+        var name = ""
+        if searchActive {
+             name = filteredAllGroups[indexPath.row]
+        } else {
+              name = allGroups[indexPath.row]
+        }
         let result = allGroupsFoto.filter{(key,value) in key.contains(name) }
         cell.allGroupLogo.image = UIImage(named: result.first?.value ?? "")
         cell.allGroupName.text = result.first?.key
@@ -48,7 +92,28 @@ class AllGroupsTableViewController: UITableViewController {
         return cell
     }
  
+    // MARK: - Private instance methods
+    
 
+
+
+    
+
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        tableView.reloadData()
+        searchActive = false
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
